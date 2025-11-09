399d936f10f7ffef4d83afe5052469e67cf8646f
Harsh Singh <harshsinghkumwar97819@gmail.com>
2025-11-09 11:03:18 +0530

    elements declared

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+const balance = document.getElementById('balance') ;
+const money_plus = document.getElementById('money_plus') ;
+const money_minus = document.getElementById('money_minus') ;
+const list = document.getElementById('balance') ;
+const form = document.getElementById('form') ;
+const text = document.getElementById('text') ;
+const amount = document.getElementById('amount') ;
+const income_text = document.getElementById('income_text') ;
+const expense_text = document.getElementById('expense_text') ;
+
+
